116f9a6d84bfcc1c78d7a4e19dfe0318187ab215
Oghenetejiri <78227200+Oghenetejiri@users.noreply.github.com>
2021-05-09 23:19:01 +0300

    Created the dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
+import Dictionay from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
        
       </header>
+      <main>
+        <Dictionay/>
+      </main>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+export default function Dictionay(){
+    return<div>
+        The dictionary
+    </div>
+}
